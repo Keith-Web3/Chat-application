77a697e2c63d29ea02ee8f416471e8634ca1ab18
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-02-23 10:37:16 +0100

    completed rtc peer connection

--- src/components/web-rtc/CallInterface.tsx
+++ src/components/web-rtc/CallInterface.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useRef } from 'react'
+import AgoraRTM, { RtmClient, RtmChannel, RtmMessage } from 'agora-rtm-sdk'
 
+import { auth } from '../Data/firebase'
 import '../../sass/web-rtc/call_interface.scss'
 
 const CallInterface: React.FC = function () {
   const userOne = useRef<HTMLVideoElement>(null)
   const userTwo = useRef<HTMLVideoElement>(null)
+
+  const appId = import.meta.env.VITE_AGORA_APP_ID
+  const token = ''
+  const uid = auth.currentUser!.uid
+
+  let client: RtmClient
+  let channel: RtmChannel
 
   let localStream: MediaStream
   let remoteStream: MediaStream
@@ -21,26 +30,67 @@ const CallInterface: React.FC = function () {
     ],
   }
 
+  const handleMessageFromPeer = async function (
+    message: RtmMessage,
+    memberId: string
+  ) {
+    const parsedMessage: RtmMessage & { type: string; [prop: string]: any } =
+      JSON.parse(message.text!)
+    if (parsedMessage.type === 'offer') {
+      createAnswer(memberId, parsedMessage.offer)
+    }
+    if (parsedMessage.type === 'answer') {
+      addAnswer(parsedMessage.answer)
+    }
+    if (parsedMessage.type === 'candidate') {
+      if (peerConnection) {
+        peerConnection.addIceCandidate(parsedMessage.candidate)
+      }
+    }
+    console.log('Message:', message)
+  }
+
+  const handleUserJoined = async function (memberId: string) {
+    console.log('WORKS!')
+    console.log('A new user joined the channel:', memberId)
+    createOffer(memberId)
+  }
+
   const init = async function () {
+    client = AgoraRTM.createInstance(appId)
+    await client.login({ uid })
+
+    channel = client.createChannel('main8')
+    await channel.join()
+    channel.on('MemberJoined', handleUserJoined)
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
     localStream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: false,
     })
 
     userOne.current!.srcObject = localStream
-    createOffer()
   }
-  const createOffer = async function () {
+
+  const createPeerConnection = async function (memberId: string) {
     peerConnection = new RTCPeerConnection()
     remoteStream = new MediaStream()
 
     userTwo.current!.srcObject = remoteStream
+
+    if (!localStream) {
+      localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      })
+
+      userOne.current!.srcObject = localStream
+    }
+
     localStream.getTracks().forEach(track => {
       peerConnection.addTrack(track, localStream)
     })
-
-    const offer = await peerConnection.createOffer()
-    await peerConnection.setLocalDescription(offer)
 
     peerConnection.addEventListener('track', e => {
       e.streams[0].getTracks().forEach(track => {
@@ -49,11 +99,47 @@ const CallInterface: React.FC = function () {
     })
     peerConnection.addEventListener('icecandidate', e => {
       if (e.candidate) {
-        console.log('New ICE Candidate', e.candidate)
+        client.sendMessageToPeer(
+          {
+            text: JSON.stringify({ type: 'candidate', candidate: e.candidate }),
+          },
+          memberId
+        )
       }
     })
+  }
 
-    console.log(offer)
+  const createOffer = async function (memberId: string) {
+    await createPeerConnection(memberId)
+
+    const offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+
+    client.sendMessageToPeer(
+      { text: JSON.stringify({ type: 'offer', offer: offer }) },
+      memberId
+    )
+  }
+
+  const createAnswer = async function (
+    memberId: string,
+    offer: RTCSessionDescriptionInit
+  ) {
+    await createPeerConnection(memberId)
+
+    await peerConnection.setRemoteDescription(offer)
+    const answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer(
+      { text: JSON.stringify({ type: 'answer', answer: answer }) },
+      memberId
+    )
+  }
+  const addAnswer = async function (answer: RTCSessionDescriptionInit) {
+    if (!peerConnection.currentRemoteDescription) {
+      peerConnection.setRemoteDescription(answer)
+    }
   }
 
   useEffect(() => {
