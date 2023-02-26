0d7d73e085c8604d08c565c2dd97e30d0190d471
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-02-26 18:15:21 +0100

    edited server folder

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ const generateAccessToken = function (req, resp) {
     privilegeExpireTime
   )
 
-  return resp.json({ token: token })
+  return resp.json({ token: token, appId: APP_ID, appCert: APP_CERTIFICATE })
 }
 
 app.get('/', nocache, generateAccessToken)
