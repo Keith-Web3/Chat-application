c388fa39d0d068575444962a70db7d2be4dc8589
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2023-03-03 11:21:10 +0100

    reduced openai completion temperature

--- src/components/Utils/chatFunctions.tsx
+++ src/components/Utils/chatFunctions.tsx
@@ -74,7 +74,7 @@ export const sendMessage = async function (
       model: 'text-davinci-003',
       prompt,
       max_tokens: 100,
-      temperature: 1.5,
+      temperature: 0.8,
     })
     await updateDoc(channelRef, {
       messages: arrayUnion({
